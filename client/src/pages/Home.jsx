ca476f01a063ee4b46e767f51a5e180be238767a
kienprovip <lovankien@gmail.com>
2024-11-07 21:55:08 +0700

    install server project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        {/* slider */}
+        <div>
+            <img src="https://images.vexels.com/media/users/3/194700/list/aa5b7c80ff2c80f764e2cabf5492a701-buy-online-slider-template.jpg" alt="" className='w-full' />
+        </div>
+    </div>
   )
 }
